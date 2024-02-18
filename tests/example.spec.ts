b4ec7f374518d9feb2a52bdd53c3f104d2c51118
Sebastian Dickerson <sebastiandickerson12@gmail.com>
2024-02-19 09:33:14 +1000

    feat: add seach modal test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,11 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('get search modal', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click search input
+  await page.getByLabel('Search').click();
+  await page.getByPlaceholder('Search docs').click();
+});
